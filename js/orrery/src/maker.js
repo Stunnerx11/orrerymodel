586c9652255025dddd293b68e9e9a7e12c17e4bd
Stunnerx11 <168223868+Stunnerx11@users.noreply.github.com>
2024-10-06 00:10:53 +0530

    Update maker.js

--- js/orrery/src/maker.js
+++ js/orrery/src/maker.js
@@ -1,6 +1,6 @@
 import * as ORR from "./init.js";
-import * as THREE from "../../../node_modules/three/build/three.module.js";
-import { mergeBufferGeometries } from "../../../node_modules/three/examples/jsm/utils/BufferGeometryUtils.js";
+import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.153.0/build/three.module.js";
+import { mergeBufferGeometries } from "https://cdn.jsdelivr.net/npm/three@0.153.0/examples/jsm/utils/BufferGeometryUtils.js";
 import $ from "../../jquery/jquery.module.js";
 
 const gratRadius = 1000;
