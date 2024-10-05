2f73d6e3ea7f8478087e4fdfc2fabcbe29b46bae
Stunnerx11 <168223868+Stunnerx11@users.noreply.github.com>
2024-10-06 00:09:13 +0530

    Update mainloop.js

--- js/orrery/src/mainloop.js
+++ js/orrery/src/mainloop.js
@@ -1,5 +1,5 @@
 import * as ORR from "./init.js";
-import * as THREE from "../../../node_modules/three/build/three.module.js";
+import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.153.0/build/three.module.js";
 import * as TWEEN from "../../tween/tween.esm.js";
 
 export let liveData = false;
