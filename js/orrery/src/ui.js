dcefde1147aed6e43146f7fb5b1321f76812c3e6
Stunnerx11 <168223868+Stunnerx11@users.noreply.github.com>
2024-10-06 00:13:42 +0530

    Update ui.js

--- js/orrery/src/ui.js
+++ js/orrery/src/ui.js
@@ -1,6 +1,6 @@
 import * as ORR from "./init.js";
-import * as TWEEN from "../../tween/tween.esm.js";
-import $ from "../../jquery/jquery.module.js";
+import * as TWEEN from "https://cdn.jsdelivr.net/npm/@tweenjs/tween.js@18.6.4/src/TWEEN.js";
+import $ from "https://code.jquery.com/jquery-3.6.0.min.js";
 
 const planetScale = {f: 1.0};
 const planetMoons = []; // moons of the the currently focused planet
